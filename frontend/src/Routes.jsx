b537787cb482db08def52af4ea69f4db480fd556
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-09-25 12:38:00 +0200

    implement login and sign up pages UI

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,7 @@
-import LoginPage from '../src/pages/LoginPage.jsx'
+import LoginPage from '../src/pages/LoginPage'
+import SignUpPage from '../src/pages/SignUpPage'
 
 export {
     LoginPage,
+    SignUpPage,
 };
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -1,22 +1,53 @@
-import { React, useState } from "react";
+import { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
+import { RxAvatar } from "react-icons/rx";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const handleSubmit = () => {
+    console.log("ffff");
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files(0);
+    setAvatar(file);
+  };
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm-w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account
+          Register as a new user
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-18">
           <form className="space-y-6">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Full name
+              </label>
+              <div className="mt-1">
+                <input
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  type="text"
+                  name="name"
+                  autoComplete="name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            </div>
             <div>
               <label
                 htmlFor="email"
@@ -70,26 +101,37 @@ const Login = () => {
             </div>
             <div className={`${styles.normalFlex} justify-between`}>
               <div className={`${styles.normalFlex}`}>
-                <input
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  type="checkbox"
-                  name="remember-me"
-                  id="remember-me"
-                />
                 <label
-                  className="ml-2 block text-sm text-gray-900"
-                  htmlFor="remember-me"
-                >
-                  Remember me
-                </label>
-              </div>
-              <div className="text-sm">
-                <a
-                  className="font-medium text-blue-600 hover:text-blue-500"
-                  href="/forgot-password"
-                >
-                  Forgot your password ?
-                </a>
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="avatar"
+                ></label>
+                <div className="mt-2 flex items-center">
+                  <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                    {avatar ? (
+                      <img
+                        className="h-full w-full object-cover rounded-full"
+                        src={URL.createObjectURL(avatar)}
+                        alt="avatar"
+                      />
+                    ) : (
+                      <RxAvatar className="h-8 w-8" />
+                    )}
+                  </span>
+                  <label
+                    className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-200"
+                    htmlFor="file-input"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      className="sr-only"
+                      type="file"
+                      name="avatar"
+                      id="file-input"
+                      accept=".jpg, .jpeg, .png"
+                      onChange={handleFileInputChange}
+                    />
+                  </label>
+                </div>
               </div>
             </div>
             <div>
@@ -100,9 +142,9 @@ const Login = () => {
                 Submit
               </button>
               <div className={`${styles.normalFlex} w-full py-4`}>
-                <h4>Not have any account ?</h4>
-                <Link className="text-blue-600 pl-2" to="/sign-up">
-                  Sign Up
+                <h4>Already have an account ?</h4>
+                <Link className="text-blue-600 pl-2" to="/login">
+                  Sign In
                 </Link>
               </div>
             </div>
@@ -113,4 +155,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
