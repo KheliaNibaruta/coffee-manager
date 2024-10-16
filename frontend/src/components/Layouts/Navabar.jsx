78e0d23929818e399eac9b82937413d53876d954
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-10-16 17:50:55 +0200

    implement Hero section and categories frontend

--- frontend/src/components/Layouts/Navabar.jsx
+++ frontend/src/components/Layouts/Navabar.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Header from './Header'
-import styles from '../../styles/styles'
-import { navItems } from '../../static/data'
-import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
+import { navItems } from '../../static/data';
+import { Link } from 'react-router-dom';
 
-const Navabar = ({active}) => {
+const Navabar = ({ active }) => {
   return (
     <div className={`${styles.normalFlex}`}>
       {
-        navItems && navItems.map((i,index) => (
-          // eslint-disable-next-line react/jsx-key
-          <div className="flex">
+        navItems && navItems.map((i, index) => (
+          <div key={index} className="flex">
             <Link to={i.url}
               className={`${active === index + 1 ? "text-[#17dd1f]" : "text-[#fff]"} font-[500] px-6 cursor-pointer}`}>
               {i.title}
@@ -19,7 +17,12 @@ const Navabar = ({active}) => {
         ))
       }
     </div>
-  )
-}
+  );
+};
 
-export default Navabar
+// Validation des props avec PropTypes
+Navabar.propTypes = {
+  active: PropTypes.number.isRequired,
+};
+
+export default Navabar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import Header from '../components/Layouts/Header'
+import Hero from "../components/Route/Hero/Hero"
+import Categories from "../components/Route/Hero/Categories.jsx"
 
 const HomePage = () => {
   return (
     <div>
         <Header activeHeading={1} />
+        <Hero />
+        <Categories />
     </div>
   )
 }
